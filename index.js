55925d601fd176befc2e6978f85a421729abe0d7
shahzodotaboyev <shoxzodbek15@gmail.com>
2024-10-20 10:25:24 +0500

    1masala

---
+++ index.js
@@ -0,0 +1,10 @@
+// let anim = {
+//     name: 'tiger',
+//     speed: '56-64 km/h',
+//     limitAge: '11 year',
+
+//     get info() {
+//         return `${ this.name }, ${ this.speed }, ${ this.limitAge }`
+//     }
+// }
+// console.log(anim.info);
